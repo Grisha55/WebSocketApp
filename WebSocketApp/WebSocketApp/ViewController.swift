9b9121d6ba7e1b97cd7e92ded2b372c56663427e
Grigory <grishavinyar@icloud.com>
2022-09-02 20:46:19 +0300

    Initial commit

---
+++ WebSocketApp/WebSocketApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebSocketApp
+//
+//  Created by Григорий Виняр on 02/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.backgroundColor = .blue
+  }
+
+
+}
+
