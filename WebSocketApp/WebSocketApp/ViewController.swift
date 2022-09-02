098dab44b9c99df8fe09caa4f095ce7b1692e80e
Grigory <grishavinyar@icloud.com>
2022-09-02 22:45:23 +0300

    Created UI for ViewController

--- WebSocketApp/WebSocketApp/ViewController.swift
+++ WebSocketApp/WebSocketApp/ViewController.swift
@@ -9,12 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  // MARK: - Properties
+  private let disconnectButton: UIButton = {
+    let button = UIButton()
+    button.backgroundColor = .red
+    button.setTitle("Disconnect", for: .normal)
+    button.titleLabel?.font = UIFont.systemFont(ofSize: 30, weight: .bold)
+    return button
+  }()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
     view.backgroundColor = .blue
+    
+    setupDisconnectButton()
+  }
+  
+  // MARK: - Methods
+  private func setupDisconnectButton() {
+    view.addSubview(disconnectButton)
+    disconnectButton.addTarget(self, action: #selector(disconnectButtonAction), for: .touchUpInside)
+    
+    disconnectButton.translatesAutoresizingMaskIntoConstraints = false
+    disconnectButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    disconnectButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
   }
 
-
+  @objc
+  func disconnectButtonAction() {
+    
+  }
+  
 }
 
