b464c7e8774fcc7c2e620dcf515c48433a6b37fa
Grigory <grishavinyar@icloud.com>
2022-09-02 23:47:45 +0300

    Created WebSocketService

--- WebSocketApp/WebSocketApp/ViewController.swift
+++ WebSocketApp/WebSocketApp/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ViewController: UIViewController {
 
   // MARK: - Properties
+  private var webSocket: URLSessionWebSocketTask?
+  
   private let disconnectButton: UIButton = {
     let button = UIButton()
     button.backgroundColor = .red
@@ -24,9 +26,75 @@ class ViewController: UIViewController {
     view.backgroundColor = .blue
     
     setupDisconnectButton()
+    setupWebSocket()
   }
   
   // MARK: - Methods
+  func setupWebSocket() {
+    // Session
+    let session = URLSession(configuration: .default, delegate: self, delegateQueue: OperationQueue())
+    
+    // Server API
+    guard let url = URL(string: "wss://demo.piesocket.com/v3/channel_1?api_key=VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV&notify_self") else { return }
+    
+    // Socket
+    webSocket = session.webSocketTask(with: url)
+    
+    // Connect and handles handshake
+    webSocket?.resume()
+  }
+  
+  // MARK: Receive
+  func receive() {
+    let workItem = DispatchWorkItem { [weak self] in
+      
+      self?.webSocket?.receive(completionHandler: { result in
+        
+        switch result {
+        case .success(let message):
+          
+          switch message {
+            
+          case .data(let data):
+            print("Data receive \(data)")
+            
+          case .string(let strMessage):
+            print("String received \(strMessage)")
+            
+          default:
+            break
+          }
+        case .failure(let error):
+          print("Error Receiving \(error)")
+        }
+        
+        // Creates the Recurrsion
+        self?.receive()
+      })
+    }
+    
+    DispatchQueue.global().asyncAfter(deadline: .now() + 1, execute: workItem)
+  }
+  
+  // MARK: Send
+  func send() {
+    let workItem = DispatchWorkItem {
+      
+      // Send webSocket
+      self.webSocket?.send(URLSessionWebSocketTask.Message.string("Hello"), completionHandler: { error in
+        
+        if error == nil {
+          // if error is nil we will continue to send messages else we will stop
+          self.send()
+        } else {
+          print(error)
+        }
+      })
+    }
+    
+    DispatchQueue.global().asyncAfter(deadline: .now() + 3, execute: workItem)
+  }
+  
   private func setupDisconnectButton() {
     view.addSubview(disconnectButton)
     disconnectButton.addTarget(self, action: #selector(disconnectButtonAction), for: .touchUpInside)
@@ -38,8 +106,22 @@ class ViewController: UIViewController {
 
   @objc
   func disconnectButtonAction() {
-    
+    webSocket?.cancel(with: .goingAway, reason: "You've Closed The Connection".data(using: .utf8))
   }
   
 }
 
+// MARK: - URLSessionWebSocketDelegate
+extension ViewController: URLSessionWebSocketDelegate {
+  
+  func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didOpenWithProtocol protocol: String?) {
+    print("Connected to server")
+    self.receive()
+    self.send()
+  }
+  
+  func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didCloseWith closeCode: URLSessionWebSocketTask.CloseCode, reason: Data?) {
+    print("Disconnect from server \(reason)")
+  }
+  
+}
